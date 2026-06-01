53e8954b76af783797c5d96306adde2610bda3b7
Mukesh K.C <mukeshkc2024@gmail.com>
2026-06-01 08:01:59 +0545

    day2: conditional statement

--- day2.py
+++ day2.py
@@ -26,3 +26,55 @@ print("Only in one course:", course_A ^ course_B)
 scores_with_doplicates = [85, 92, 85, 78, 92, 95, 85]
 unique_scores = list(scores_with_doplicates)
 print("Unique scores:", unique_scores)
+
+print("\n--- Dictionaries ---")
+print("="*50)  
+# Dictionaries store key-value pairs and are very useful for structured data
+# Dictionaries store data with keys
+student = {
+    "name": "Alice",
+    "age": 20,
+    "scores": [85,92, 78],
+    "department": "BCA",
+    "is_active": True
+}
+
+print("Student Dictionary:")
+print(student)
+
+# Accessing values
+print("\nStudent name:", student['name'])
+print("Student scores:", student['scores'])
+print("Average scores:", sum(student['scores'])/len(student["scores"]))
+
+# Adding/updating values
+student["grade"] = "A"
+student["age"] = 21
+student["hobbies"] = ["Cording", "Dancaing"]
+print("\nAfter update:", student)
+
+alis_scores = student['scores']
+
+alice_passing_scores = [score for score in alis_scores if score >= 80]
+print("\n Alice Passing scores (>=80):", alice_passing_scores)
+
+# Conditional statements
+# Function to determine grade based on score
+def get_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+        
+# Test teh function 
+test_scores = [95, 85, 75, 65, 55]
+for score in test_scores:
+    grade = get_grade(score)
+    print("Score:", score, "")
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print("\nStudents in either courses:", course_A | course_B)
